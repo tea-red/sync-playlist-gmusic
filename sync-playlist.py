1937d1ed3ac06226ad4fe8d94727854f01b2824b
tea-red <47109630+tea-red@users.noreply.github.com>
2020-05-04 06:32:43 +0900

    使用するライブラリをitunesLibraryへ変更

--- sync-playlist.py
+++ sync-playlist.py
@@ -1,4 +1,4 @@
-from libpytunes import Library
+from itunesLibrary import library
 from gmusicapi import Mobileclient
 from dotenv import load_dotenv
 import time
@@ -11,96 +11,106 @@ load_dotenv()
 
 # itunes music library xmlのパスを取得
 ITUNES_MUSIC_LIBLARY_XML_PATH = os.environ["ITUNES_MUSIC_LIBLARY_XML_PATH"]
+SYNC_PLAYLIST_NAME = os.environ["SYNC_PLAYLIST_NAME"]
 MAX_GPLAYLIST_SONGS_NUM = 1000
+
+print("sync playlistName = {p}".format(p=SYNC_PLAYLIST_NAME))
 
 mc = Mobileclient()
 mc.oauth_login(mc.FROM_MAC_ADDRESS)
 
+print('google play music login success')
+
+print('google play music load start')
+
 # google play musicの曲リストを取得
 gLibrary = mc.get_all_songs()
+
+print('google play music library loaded')
 
 # google play musicのプレイリストとそのコンテンツを取得
 gPlaylistsAndContents = mc.get_all_user_playlist_contents()
 
+print('google play music playlists loaded')
+
+print('itunes load start')
+
 # itunesのライブラリファイル読み込み
-iLibrary = Library(ITUNES_MUSIC_LIBLARY_XML_PATH)
+iLibrary = library.parse(ITUNES_MUSIC_LIBLARY_XML_PATH)
+
+print('itunes playlists loaded')
 
 # プレイリスト一覧を取得
-iAllPlaylists = iLibrary.il['Playlists']
+iPlaylist = iLibrary.getPlaylist(SYNC_PLAYLIST_NAME)
 
-# プレイリスト一覧からスマートプレイリスト、自作のプレイリスト以外を除く
-iPlaylists = filter(lambda v: (('Distinguished Kind') not in v) and (('Master') not in v) , iAllPlaylists)
+# プレイリスト名を表示
+print("playlistName = {p}".format(p=iPlaylist.itunesAttibutes['Name']))
 
-print('load end')
+# 曲を登録するプレイリストID保存用変数定義
+gPlaylistId = None
 
-# itunesのプレイリストごとに
-for iPlaylist in iPlaylists:
-    # プレイリスト名を表示
-    print("playlistName = {p}".format(p=iPlaylist['Name']))
+# google play musicの一致するプレイリストを取得
+filteredPlaylistsAndContentsIter = filter(lambda v: v['name'] == iPlaylist.itunesAttibutes['Name'], gPlaylistsAndContents)
 
-    # 曲を登録するプレイリストID保存用変数定義
-    gPlaylistId = None
+# 一致するプレイリストが見つかったら
+for filteredPlaylistAndContents in filteredPlaylistsAndContentsIter:
+    # googleplaymusicのプレイリストIDを取得
+    gPlaylistId = filteredPlaylistAndContents['id']
 
-    # google play musicの一致するプレイリストを取得
-    filteredPlaylistsAndContentsIter = filter(lambda v: v['name'] == iPlaylist['Name'], gPlaylistsAndContents)
+    # プレイリストに含まれるトラックidのリストを取得して
+    includeTrackIdsIter = map(lambda v: v['id'], filteredPlaylistAndContents['tracks'])
 
-    # 一致するプレイリストが見つかったら
-    for filteredPlaylistAndContents in filteredPlaylistsAndContentsIter:
-        # googleplaymusicのプレイリストIDを取得
-        gPlaylistId = filteredPlaylistAndContents['id']
+    # プレイリストから消去
+    mc.remove_entries_from_playlist([trackId for trackId in includeTrackIdsIter])
+    break
 
-        # プレイリストに含まれるトラックidのリストを取得して
-        includeTrackIdsIter = map(lambda v: v['id'], filteredPlaylistAndContents['tracks'])
+# プレイリストの登録曲数がgoogle play musicの最大登録数よりも多い場合は
+iPlaylistItems = len(iPlaylist.items)
+if(iPlaylistItems > MAX_GPLAYLIST_SONGS_NUM):
+    # オーバーしている旨のメッセージを出す
+    print("playlistName = {p} is over max playlist song size :{n}".format(p=iPlaylist.itunesAttibutes['Name'], n=iPlaylistItems))
+    # プレイリストが存在している場合は削除する。
+    if gPlaylistId != None:
+        mc.delete_playlist(gPlaylistId)
+    # return
+    exit()
 
-        # プレイリストから消去
-        mc.remove_entries_from_playlist([trackId for trackId in includeTrackIdsIter])
-        break
+# プレイリストが見つからなかったら新たに作成する。
+if gPlaylistId == None:
+    gPlaylistId = mc.create_playlist(iPlaylist.itunesAttibutes['Name'])
 
-    # プレイリストの登録曲数がgoogle play musicの最大登録数よりも多い場合は
-    iPlaylistItems = len(iPlaylist['Playlist Items'])
-    if(iPlaylistItems > MAX_GPLAYLIST_SONGS_NUM):
-        # オーバーしている旨のメッセージを出す
-        print("playlistName = {p} is over max playlist song size :{n}".format(p=iPlaylist['Name'], n=iPlaylistItems))
-        # プレイリストが存在している場合は削除する。
-        if gPlaylistId != None:
-            mc.delete_playlist(gPlaylistId)
-        continue
+# google play musicのsong idを取得
+def getGSongId(songName, artist, album):
+    # itunesのタイトル、アルバム、アーティストに一致するgoogle play musicのトラックを取得
+    foundGTrack = [
+        gTrack for gTrack in gLibrary
+        if gTrack['title'] == songName
+        and gTrack['album'] == album
+        and gTrack['artist'] == artist
+    ]
 
-    # プレイリストが見つからなかったら新たに作成する。
-    if gPlaylistId == None:
-        gPlaylistId = mc.create_playlist(iPlaylist['Name'])
+    # (1曲に特定できない、一致する曲がない)
+    if(len(foundGTrack) != 1):
+        # メッセージを出力
+        print("not found {n}".format(n=songName))
+        return None
 
-    # itunesのプレイリストに含まれるTrack IDのリストを取得
-    iPlTrIds = map(lambda v: v['Track ID'], iPlaylist['Playlist Items'])
+    return foundGTrack[0]['id']
 
-    # itunesのtrackidからgoogle play musicのsong idリストを取得
-    def getGSongIdFromITrackId(iTrackId):
-        # 紐づくitunesの曲情報を取得
-        iSong = iLibrary.songs.get(iTrackId)
+# 登録対象のgoogle play musicのidリストを取得
+addGTrackIdIter = map(
+    lambda song: getGSongId(
+        song.itunesAttibutes['Name'],
+        song.itunesAttibutes['Artist'],
+        song.itunesAttibutes['Album'],
+    ),
+    iPlaylist.items
+)
+addGTrackIdList = [id for id in addGTrackIdIter]
 
-        # itunesのタイトル、アルバム、アーティストに一致するgoogle play musicのトラックを取得
-        foundGTrack = [
-            gTrack for gTrack in gLibrary
-            if gTrack['title'] == iSong.name
-            and gTrack['album'] == iSong.album
-            and gTrack['artist'] == iSong.artist
-        ]
+# スリープ
+time.sleep(1)
 
-        # (1曲に特定できない、一致する曲がない)
-        if(len(foundGTrack) != 1):
-            # メッセージを出力
-            print("not found {n}".format(n=iSong.name))
-            return None
-
-        return foundGTrack[0]['id']
-
-    # 登録対象のgoogle play musicのidリストを取得
-    addGTrackIdIter = map(lambda id: getGSongIdFromITrackId(id),iPlTrIds)
-    addGTrackIdList = [id for id in addGTrackIdIter]
-
-    # スリープ
-    time.sleep(1)
-
-    # 対象のidを登録対象のプレイリストへ追加
-    addResult = mc.add_songs_to_playlist(gPlaylistId, addGTrackIdList)
+# 対象のidを登録対象のプレイリストへ追加
+addResult = mc.add_songs_to_playlist(gPlaylistId, addGTrackIdList)
 
